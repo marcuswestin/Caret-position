8da5cc685afe6fd4f7e5571208f2c968eeac9294
Marcus Westin <marcus.westin@gmail.com>
2015-02-14 12:17:27 -0500

    Clean up export code

--- caret-position.js
+++ caret-position.js
@@ -1,21 +1,11 @@
 "use strict"
-// Module export pattern from
-// https://github.com/umdjs/umd/blob/master/returnExports.js
+// Module export pattern from https://github.com/umdjs/umd/blob/master/returnExports.js
 ;(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.caretPosition = factory();
-  }
+    if (typeof define === 'function' && define.amd) { define([], factory) }
+    else if (typeof exports === 'object') { module.exports = factory() }
+    else { root.caretPosition = factory() }
 }(this, function () {
-	return {
+	var caretPosition = {
 		get: getPosition,
 		set: setPosition
 	}
@@ -78,6 +68,8 @@
 			range.select()
 		}
 	}
+	
+	return caretPosition
 }));
 
 
